f1252e4c715e20a0c5e9d910eba3d8abe94582f9
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-07-02 18:19:23 +0530

    Time: 3 ms (29.85%), Space: 16.4 MB (16.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -18,7 +18,23 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        
-        return findPathSum(grid, m-1, n-1, dp);
+        // return findPathSum(grid, m-1, n-1, dp);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int up=1e9, left=1e9;
+                    if(i>0)
+                        up = grid[i][j] + dp[i-1][j];
+                    if(j>0)
+                        left = grid[i][j] + dp[i][j-1];
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
