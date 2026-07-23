603ca7b1faa09dbeeea37d99afb5f546fce6d073
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-07-23 16:04:48 +0530

    Time: 1382 ms (58.39%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+select v.customer_id, count(v.visit_id) as count_no_trans from Visits as v
+LEFT JOIN
+Transactions as t
+on
+v.visit_id = t.visit_id where t.transaction_id is null
+GROUP BY customer_id;
+
