b4a12c0306dbe7cca23123ae483d7fd703194e4c
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-08-06 14:28:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        
+        while(n != 0){
+            int temp = n;
+            int mul = 1;
+            while(temp>0){
+                mul *= temp%10;
+                temp /= 10;
+            }
+            if(mul%t==0)
+                return n;
+            else
+                n++;
+        }
+        return 0;
+    }
+};
