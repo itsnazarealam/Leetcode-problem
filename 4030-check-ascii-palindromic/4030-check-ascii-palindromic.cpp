d1555abb6b43f2fc9691b6ee8fdc896946aea0cb
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-08-23 16:39:58 +0530

    Time: 0 ms (100%), Space: 9.2 MB (63.39%) - LeetHub

---
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string intToBin(int num){
+        string ans = "";
+        for(int i=7; i>=0; i--){
+            if(num & (1<<i)){
+                ans += '1';
+            }
+            else{
+                ans += '0';
+            }
+        }
+        return ans;
+    }
+    bool isPalindromic(string s) {
+        string output = "";
+        for(int i=0; i<s.size(); i++){
+            output += intToBin(s[i]-0);
+
+        }
+        int l=0, r=output.size()-1;
+        while(l<=r){
+            if(output[l] != output[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
