069aa3029af5c438c42dc4bfec4919daa90738b2
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-09-16 18:08:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.89%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int even=0, odd=0;
+        for(int i=1; i<=2*n; i++){
+            if(i%2==0)
+                even += i;
+            else
+                odd += i;
+        }
+        return gcd(even, odd);
     }
 };
