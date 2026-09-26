32a32374b87fc813f7e3d37c7480233c3e0e30a0
Md Nazare Alam <79821343+itsnazarealam@users.noreply.github.com>
2026-09-27 00:01:12 +0530

    Time: 579 ms (99.52%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,15 +1,16 @@
-SELECT Department, Employee, Salary
+Select Department, Employee, Salary
 FROM(
     SELECT
-        d.name AS Department,
-        e.name AS Employee,
-        e.salary AS Salary,
-        DENSE_RANK() OVER (
-            PARTITION BY e.departmentId 
-            ORDER BY e.salary DESC
-        ) AS rank_num
-    FROM Employee AS e
-    JOIN Department AS d
+        d.name Department,
+        e.name Employee,
+        e.salary as Salary,
+        Dense_Rank() Over(
+            partition by e.departmentid
+            order by e.salary Desc
+        ) as rank_num    
+    FROM
+    Employee AS e
+    INNER JOIN Department AS d
     ON e.departmentId = d.id
 ) ranked
-WHERE rank_num = 1;
+where rank_num=1;
